c2a723b9858c858e9ec3376927dc4e8f34e90307
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-09-28 11:51:32 +0500

    updating complete

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ import AiPhile as ap
 
 
 camera = cv.VideoCapture(0)
+# getting old frame 
+old_gray = cv.cvtColor(camera.read()[1], cv.COLOR_BGR2GRAY)
+cv.imshow('old_gray', old_gray)
+# cv.waitKey(0)
 start_time = time.time()
 frame_counter = 0
-fonts = cv.FONT_HERSHEY_PLAIN
 fonts =cv.FONT_HERSHEY_SIMPLEX
+# optical_flow parmamters 
+def pointSelector(event, x, y, flags, params):
+
+    global point, condition, old_point
+    if event ==cv.EVENT_LBUTTONDOWN:
+        point = np.array([x, y]).astype(int)
+        condition =True
+        old_point = np.array([[x, y]], dtype=np.float32)
 while True:
     frame_counter +=1
     ret, frame = camera.read()
