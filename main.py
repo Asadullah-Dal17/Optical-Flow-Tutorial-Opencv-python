811ce084f4e88c09b74586e2cee1f1e07bc83ade
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-09-28 20:44:39 +0500

    updating file

--- main.py
+++ main.py
@@ -5,41 +5,55 @@ import AiPhile as ap
 
 
 camera = cv.VideoCapture(0)
-# getting old frame 
+# getting old frame
+_, frame = camera.read() 
 condition =False
-old_gray = cv.cvtColor(camera.read()[1], cv.COLOR_BGR2GRAY)
+old_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 cv.imshow('old_gray', old_gray)
 start_time = time.time()
 frame_counter = 0
 fonts =cv.FONT_HERSHEY_SIMPLEX
 # optical_flow parmamters 
-parmas_dict = dict(winSize=(20, 20), maxLevel=4, criteria=(cv.TERM_CRITERIA_EPS| cv.TermCriteria_COUNT, 10, 0.01))
+lk_params = dict(winSize=(20, 20),
+                 maxLevel=4,
+                 criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.01))
+# parmas_dict = dict(winSize=(20, 20), maxLevel=4, criteria=(cv.TERM_CRITERIA_EPS| cv.TERM_CRITERIA_COUNT, 10, 0.01))
 def pointSelector(event, x, y, flags, params):
 
     global point, condition, old_point
     if event ==cv.EVENT_LBUTTONDOWN:
-        point = np.array([x, y]).astype(int)
+        # point = np.array([x, y]).astype(int)
+        point= (int(x), int(y))
         condition =True
         old_point = np.array([[x, y]], dtype=np.float32)
 
 cv.namedWindow('frame')
 cv.setMouseCallback('frame', pointSelector)
-old_points = np.array([[]])
+old_point = np.array([[]])
+point=()
+condition =False
 while True:
     frame_counter +=1
     ret, frame = camera.read()
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    old_point.astype(int)
     if condition:
+   
+        # new_point,status, error = cv.calcOpticalFlowPyrLK(
+        #     old_gray, gray_frame, old_point,None, **lk_params)
+        new_point, status, error = cv.calcOpticalFlowPyrLK(
+            old_gray, gray_frame, old_point, None, **lk_params)
         cv.circle(frame, point,5, ap.ORANGE, -1)
         cv.circle(frame, point, 7, ap.INDIGO, 2)
-        new_point,status, error = cv.calcOpticalFlowPyrLK(old_gray, gray_frame, old_point,None, **parmas_dict)
-
         old_point = new_point
-        p = new_point.astype(int)
-        x, y = p.ravel()
-        print(x, y)
-        cv.circle(frame, (x,y), 4, ap.GREEN, -1)
-        cv.circle(frame,(x,y), 7, ap.BLACK, 2)
+        # old_point = new_point
+        print(old_point)
+        new = new_point.astype(int)
+        
+        x1, y1 = new.ravel()
+        print(x1, y1)
+        cv.circle(frame, (x1,y1), 4, ap.GREEN, -1)
+        cv.circle(frame,(x1,y1), 7, ap.BLACK, 2)
         
     fps = frame_counter/(time.time() - start_time)
     ap.textBGoutline(frame, f'FPS: {round(fps,1)}', (30, 40), fonts, scaling= 0.5, thickness=1)
