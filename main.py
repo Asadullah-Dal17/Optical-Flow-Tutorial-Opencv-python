4796995f66b0b8807aa5d9f8bd460ea19f753566
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-09-28 10:10:56 +0500

    updating file :green_heart:

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ camera = cv.VideoCapture(0)
 start_time = time.time()
 frame_counter = 0
 fonts = cv.FONT_HERSHEY_PLAIN
+fonts =cv.FONT_HERSHEY_SIMPLEX
 while True:
     frame_counter +=1
     ret, frame = camera.read()
 
     # ap.textBG(frame, 'Computer Vision Test', (40, 40))
-    ap.textBG(frame, 'AiPhile', (90, 90), fonts, scaling= 4, thickness=4)
+    ap.textBGoutline(frame, 'AiPhile', (90, 90), fonts, scaling= 2, thickness=4)
     cv.imshow('frame', frame)
     key = cv.waitKey(1)
     if key ==ord('q'):
